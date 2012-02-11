6cb63838415621bf1b4341ca38616a0fe2092fea
Aaron Zinman <aaron@azinman.com>
2012-02-10 17:13:03 -0800

    CFUUIDCreateFromString is very particular about its format

--- NSUUID.m
+++ NSUUID.m
@@ -14,6 +14,19 @@
 {
     if (self = [super init])
     {
+        if (uuidStr.length == 32)
+        {
+            // Need to add hypens because CFUUIDCreateFromString is unnecessarily stupid.
+            info(@"Original: %@", uuidStr);
+            uuidStr = [NSString stringWithFormat:@"%@-%@-%@-%@-%@", 
+                       [uuidStr substringWithRange:NSMakeRange(0, 8)],
+                       [uuidStr substringWithRange:NSMakeRange(8, 4)],
+                       [uuidStr substringWithRange:NSMakeRange(12, 4)],
+                       [uuidStr substringWithRange:NSMakeRange(16, 4)],
+                       [uuidStr substringWithRange:NSMakeRange(20, 12)]];
+            info(@"awesome verision: %@", uuidStr);
+        }
+        NSAssert(uuidStr.length == 36, @"UUID was an invalid length; must be 36 characters");
         CFUUIDRef u = CFUUIDCreateFromString(NULL, (__bridge CFStringRef) uuidStr);
         bytes = CFUUIDGetUUIDBytes(u);
         CFRelease(u);
